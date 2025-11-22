e2abfcfa47aa6fa5cf212d800b49cdd858f8ae9d
astheone <sulajalvi10@gmail.com>
2025-11-22 12:35:08 +0000

    Task 16: list all records where name is not null or empty

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Script that lists all records where name is not null or empty, ordered by score descending
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
